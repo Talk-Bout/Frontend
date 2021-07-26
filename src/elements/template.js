b9a4808efeedef6b0a50193673b0fe645011bbd0
leedmeen <leedmeen@gmail.com>
2021-07-26 10:56:31 +0900

    프로젝트: CRA, 디렉토리, 템플릿 생성

---
+++ src/elements/template.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Template = (props) => {
+
+  return (
+    <React.Fragment>
+      <div></div>
+    </React.Fragment>
+  )
+};
+
+export default Template;
---
+++ src/redux/modules/template.js
@@ -0,0 +1,43 @@
+import {createAction, handleActions} from "redux-actions";
+import {produce} from 'immer';
+import {history} from '../configureStore';
+
+// 액션타입
+const SET_NEWS = 'SET_NEWS';
+
+// 액션생성함수
+const setNews = createAction(SET_NEWS, (news_list) => ({news_list}));
+
+// 기본값 정하기
+const initialState = {
+    list: [],
+};
+
+// 액션함수
+const setNewsDB = () => {
+    return function (dispatch) {
+        const axios = require('axios');
+        axios.get('url').then((response) => {
+                dispatch(setNews(response.data));
+            })
+            .catch((err) => {
+                console.log(`에러 발생: ${err}`);
+            });
+    };
+};
+
+export default handleActions({
+    [SET_NEWS]: (state, action) => produce(state, (draft) => {
+        draft.list = [...action.payload.news_list];
+    }),
+}, initialState);
+
+
+// 액션 생성자
+const actionCreators = {
+    setNewsDB,
+}
+
+export {
+    actionCreators
+};

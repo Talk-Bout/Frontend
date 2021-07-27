d89f737b6f0b2c153ab03015cd2962b82df6e772
leedmeen <leedmeen@gmail.com>
2021-07-27 19:24:48 +0900

    디자인: ReviewWrite 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import ReviewWrite from './pages/ReviewWrite';
 
 function App() {
   return (
     <React.Fragment>
+      <ReviewWrite></ReviewWrite>
     </React.Fragment>
   );
 }

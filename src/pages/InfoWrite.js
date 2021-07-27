3923cfda5951ecce47f01a2096c120db7633eb12
dahyunyang <dahyunyang.0824@gmail.com>
2021-07-27 16:47:03 +0900

    디자인: Input 컴포넌트 요소 추가, Info 페이지 1차 뷰"
    디자인: Input 컴포넌트 요소 추가, Info 페이지 1차 뷰

---
+++ src/pages/InfoWrite.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Grid, Text, Button } from '../elements';
+
+const InfoWrite = (props) => {
+  return (
+    <React.Fragment>
+      <Grid padding="10%" backgroundColor="#E5E5E5"></Grid>
+    </React.Fragment>
+  );
+};
+
+export default InfoWrite;

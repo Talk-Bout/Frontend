4a72556574eead6631ae3c91fd86300dc17e4f8c
leedmeen <leedmeen@gmail.com>
2021-07-29 22:44:01 +0900

    기능추가: 게시글 수정하기 API axios 적용

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ function App() {
         <Route path="/review/list" exact component={ReviewList} />
         <Route path="/review/detail/:id" exact component={ReviewDetail} />
         <Route path="/review/write" exact component={ReviewWrite} />
+        <Route path="/review/write/:id" exact component={ReviewWrite} />
         <Route path="/mypage" exact component={Mypage} />
         <Route path="/mypage/edit" exact component={MyPageEdit} />
       </ConnectedRouter>
--- src/pages/ReviewWrite.js
+++ src/pages/ReviewWrite.js
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { Button, Grid, Input, Text } from '../elements';
+import {history} from '../redux/ConfigureStore';
+import {actionCreators as postActions} from '../redux/modules/post';
 
 const ReviewWrite = (props) => {
+  const dispatch = useDispatch();
+  const [edit_mode, setEditMode] = useState(false);
+  const post_id = props.match.params.id;
+  useEffect(() => {
+    if (post_id) {
+      setEditMode(true);
+    }
+  }, []);
+  const post_list = useSelector(state => state.post.list);
+  const old_post = post_list.find((post) => post.postId == post_id);
+  console.log(post_list, old_post);
+  const titleInput = useRef(null);
+  const contentInput = useRef(null);
+  const addPost = () => {
+    const new_post = {
+      postId: post_id,
+      title: titleInput.current.value,
+      content: contentInput.current.value,
+      nickname: 'username',
+      category: 'testing',
+    }
+    if (edit_mode) {
+      dispatch(postActions.editPostDB(new_post));
+    } else {
+      dispatch(postActions.addPostDB(new_post));
+    }
+  }
 
   return (
     <React.Fragment>
@@ -10,14 +40,14 @@ const ReviewWrite = (props) => {
         <Grid backgroundColor='#fff' width='45vw' height='65vh' margin='auto'>
           <WindowInner>
             <HeaderBox>
-              <Button width='7%'> X </Button>
-              <Text margin='0 13vw' fontSize='2vh' fontWeight='700' lineHeight='4vh'>부트캠프 리뷰작성</Text>
-              <Button width='7%'>등록</Button>
+              <Button width='7%' _onClick={() => history.push('/review/list')}> X </Button>
+              <Text margin='0 11vw' fontSize='2vh' fontWeight='700' lineHeight='4vh'>부트캠프 리뷰작성</Text>
+              <Button width='7%' _onClick={() => addPost()}>{edit_mode ? '수정' : '등록'}</Button>
             </HeaderBox>
             <hr style={{margin: '1vh 0', borderTop: '1px solid #eee'}}/>
             <BodyBox>
               <Row>
-                <Key>부트캠프명</Key><input placeholder='부트캠프명을 입력해주세요'></input>
+                <Key>부트캠프명</Key><input type='text' placeholder='부트캠프명을 입력해주세요' defaultValue={edit_mode ? old_post.title : null} ref={titleInput}></input>
               </Row>
               <Row>
                 <Key>기수</Key>
@@ -33,7 +63,7 @@ const ReviewWrite = (props) => {
               </Row>
               <Row>
                 <Key>부트캠프의 장점</Key>
-                <textarea placeholder='부트캠프의 장점을 입력해주세요' rows='7' cols='50' style={{resize: 'none'}}></textarea>
+                <textarea placeholder='부트캠프의 장점을 입력해주세요' defaultValue={edit_mode ? old_post.content : null} ref={contentInput} rows='7' cols='50' style={{resize: 'none'}}></textarea>
               </Row>
               <Row>
                 <Key>부트캠프의 단점</Key>

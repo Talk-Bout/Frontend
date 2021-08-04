6a784d1052ae99550cbcf30642f80860d4823854
dahyunyang <dahyunyang.0824@gmail.com>
2021-08-04 10:19:28 +0900

    디자인 : 회원가입 뷰 수정 중

--- src/components/SmallWindow.js
+++ src/components/SmallWindow.js
@@ -1,36 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Button, Grid, Input, Text } from '../elements';
-import {history} from '../redux/ConfigureStore';
+import { history } from '../redux/ConfigureStore';
 
 const SmallWindow = (props) => {
-  const {children} = props;
+  const { children } = props;
   const url = window.location.pathname;
-  if (url === '/mypage/edit') {             // 마이페이지 정보수정 페이지일 때,
-    return (                                // 로그인(회원가입)보다 면적이 큼
+  if (url === '/mypage/edit') {
+    // 마이페이지 정보수정 페이지일 때,
+    return (
+      // 로그인(회원가입)보다 면적이 큼
       <React.Fragment>
-      <Grid backgroundColor='gray' height='93vh' padding='7vh 15vh'>
-        <Grid backgroundColor='#fff' width='20vw' height='70vh' margin='auto' padding='3vh 2vw'>
-          {children}
+        <Grid backgroundColor="#18181A" height="93vh" padding="7vh 15vh">
+          <Grid
+            backgroundColor="#212123"
+            width="20vw"
+            height="70vh"
+            margin="auto"
+            padding="3vh 2vw"
+          >
+            {children}
+          </Grid>
         </Grid>
-      </Grid>
-    </React.Fragment>
-    )
+      </React.Fragment>
+    );
   }
 
   return (
     <React.Fragment>
-      <Grid backgroundColor='gray' height='100vh' padding='15vh'>
-        <Grid backgroundColor='#fff' width='20vw' height='65vh' margin='auto' padding='3vh 2vw'>
+      <Grid backgroundColor="#18181A" height="100vh" padding="15vh">
+        <Grid
+          backgroundColor="#212123"
+          width="30vw"
+          height="65vh"
+          margin="auto"
+          padding="4%"
+        >
           {children}
         </Grid>
       </Grid>
     </React.Fragment>
-  )
+  );
 };
 
 SmallWindow.defaultProps = {
   children: null,
-}
+};
 
 export default SmallWindow;

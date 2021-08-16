9f0c1bae26e5cfdff0ba6454dd9b8c28fe9389bf
leedmeen <leedmeen@gmail.com>
2021-08-17 01:19:40 +0900

    기능추가: 푸터 미디어쿼리(768px) 적용

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Grid, Text } from '../elements';
 import Logo from '../image/Logo.png';
 
@@ -6,10 +7,10 @@ const Footer = (props) => {
 
   return (
     <React.Fragment>
-      <Grid height='250px' margin='80px 0 0' backgroundColor='#0E1013' padding='24px 20px'>
-        <img src={Logo} alt='로고'/>
-        <Text p fontSize='14px' fontWeight='700' color='#bdc1c6' margin='0 8px 0'>토크부트를 만든 사람들 소개</Text>
-        <Grid display='flex' justify_content='space-between' width='fit-content' margin='16px 8px 0'>
+      <Grid height='250px' margin='80px 0 0' TABmargin='64px 0 0' backgroundColor='#0E1013' padding='24px 20px'>
+        <Image src={Logo} alt='로고'/>
+        <Text p fontSize='14px' fontWeight='700' color='#bdc1c6' margin='0 8px 0' TABmargin='0 4px 0'>토크부트를 만든 사람들 소개</Text>
+        <Grid display='flex' justify_content='space-between' width='fit-content' margin='16px 8px 0' TABmargin='12px 4px 0'>
           <Text fontSize='12px' color='#bdc1c6'>© 2021 Project Talk'bout</Text>
           <Text fontSize='12px' color='#bdc1c6' margin='0 24px'>All rights reserved.</Text>
         </Grid>
@@ -17,5 +18,12 @@ const Footer = (props) => {
     </React.Fragment>
   )
 };
+
+const Image = styled.img`
+  @media screen and (max-width: 768px) {
+    height: 56px;
+    width: 140px;
+  }
+`;
 
 export default Footer;
